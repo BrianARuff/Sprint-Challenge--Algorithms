5b517ff2c039af9f1ee32c5e309c857149ffd088
Brian Ruff <thelonewolf198927@gmail.com>
2019-02-23 00:19:03 -0500

    added comments

--- robot_sort/bubble_sort.py
+++ robot_sort/bubble_sort.py
@@ -1,17 +1,22 @@
-arr = [5, 3, 25, 12, -3, 0, 1]
+arr = [5, 3, 25, 12, -3, 0, 1, 33, -4, 22, 100, -100]
 
-def advanced_bubble_sort(arr):
+def bubble_sort(arr):
+    # coun tracker to break out
     count = 0
     while count < len(arr):
         for i in range(len(arr)-1):
+            # if current item greater than next item swap
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
+                # work backwards from current index, if current item is less than the item before it then swap
             for j in range(i, 0, -1):
                 if arr[j] < arr[j-1]:
                     arr[j], arr[j-1] = arr[j-1], arr[j]
             count += 1
+        # The array should be sorted after the count equals the array length - 1
         if count == len(arr)-1:
                 break
     return arr
 
-print(advanced_bubble_sort(arr))
+print(bubble_sort(arr))
+
